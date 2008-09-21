29222aefee7d7cd32df76a677dd0d2b6667c7c87
Glen Maddern <dev@glen-ubuntu.(none)>
2008-09-21 22:50:51 +1000

    cleaning up

--- src/main/glen/jplanner/AppHandler.scala
+++ src/main/glen/jplanner/AppHandler.scala
@@ -1,14 +1,15 @@
 package glen.jplanner
 
 import org.mortbay.jetty.{Request, Response, Handler}
+import javax.servlet.http.HttpServletResponse.SC_OK
 
 object AppHandler {
-  def get() : Handler = {
-    new HandlerWrapper((target : String, request : Request, response : Response, dispatch : Int) => {
-      println("target = " + target);
-      println("request.getClass = " + request.getClass);
-      println("response.getClass = " + response.getClass);
-      println("dispatch = " + dispatch);
+  def get(): Handler = {
+    new HandlerWrapper((target: String, request: Request, response: Response, dispatch: Int) => {
+      response.setContentType("text/html")
+      response.setStatus(SC_OK)
+      response.getWriter().println(<html><head><title>{target}</title></head><body><h1>{target}</h1></body></html>)
+      request.setHandled(true)
     })
   }
 }
