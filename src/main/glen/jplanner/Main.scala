7ff69ec9db1467692a5e40ca262f35cb18f4d8e5
Glen Maddern <dev@glen-ubuntu.(none)>
2008-09-21 22:01:40 +1000

    first commit

---
+++ src/main/glen/jplanner/Main.scala
@@ -0,0 +1,24 @@
+package glen.jplanner
+
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import org.mortbay.jetty.handler.{HandlerList, ResourceHandler, DefaultHandler, AbstractHandler}
+import org.mortbay.jetty.servlet.Context
+import org.mortbay.jetty.{Handler, Server};
+import org.mortbay.jetty.handler.{DefaultHandler, HandlerList, ResourceHandler};
+
+object Main {
+  def main(args: Array[String]) {
+    val port = 8081
+    val server = new Server(port)
+
+    val resourceHandler = new ResourceHandler()
+    resourceHandler.setResourceBase("www")
+//    server.setHandlers(Array[Handler](resourceHandler, new DefaultHandler()))
+    server.setHandler(resourceHandler)
+
+    server.start()
+    server.join()
+
+    println("Hello World!");
+  }
+}
